ed9b9aa685453549067186e39a7db775496a3a83
Marivj07 <137518163+Marivj07@users.noreply.github.com>
2026-06-04 17:38:15 +0530

    Update sidebar.tsx

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -56,7 +56,7 @@ const navItems: NavItem[] = [
 
 const bottomNavItems = [
   { href: "/settings", label: "Settings", icon: Settings },
-  { href: '/about',    label: 'About MAPA',  icon: Info },
+  { href: '/about', label: 'About MAPA', icon: Info }
 ];
 
 interface SidebarProps {
